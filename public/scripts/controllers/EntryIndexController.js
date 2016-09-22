f9cc9cdd4c6de96bd2966c954d45b3d3b65525dc
Andrew <andrew.vinocur@gmail.com>
2016-09-22 09:36:31 -0700

    removes commented out seed data from entryIndexCtrl

--- public/scripts/controllers/EntryIndexController.js
+++ public/scripts/controllers/EntryIndexController.js
@@ -5,13 +5,6 @@ angular
 EntryIndexController.$inject = ['$http'];
 function EntryIndexController($http){
   var vm = this;
-  // vm.prevEntry = {};
-  // vm.prevEntries = [
-  //   {entryTitle: 'Title1', entryDate:'9/19/2016'},
-  //   {entryTitle: 'Title2', entryDate:'9/20/2016'},
-  //   {entryTitle: 'Title3', entryDate:'9/21/2016'},
-  //   {entryTitle: 'Title4', entryDate:'9/22/2016'},
-  // ];
   $http({
     method: 'GET',
     url: '/api/entries'
@@ -21,5 +14,4 @@ function EntryIndexController($http){
   }, function errorCb(error){
     console.log('error getting all entries: ', error);
   });
-
 }
