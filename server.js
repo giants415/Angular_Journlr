e694c16c0984fb96cac994fef0f3fd45d5dcf76f
Andrew <andrew.vinocur@gmail.com>
2016-09-21 19:34:08 -0700

    adds /api and json endpoins

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //CROSS ORIGIN
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 //REQUIRE DATABASE
 var db = require('./models');
 
-var entryList = [
-  {
-    entryTitle: 'Entry 1',
-    entryDate: 'September 21 2016',
-    entryBody: 'Here is the first entry. It is not great but it is functional.'
-  },
-  {
-    entryTitle: 'Entry 2',
-    entryDate: 'September 22 2016',
-    entryBody: 'Here is the second entry. It is not great as great as the first but it is also functional.'
-  }
-];
+// var entryList = [
+//   {
+//     entryTitle: 'Entry 1',
+//     entryDate: 'September 21 2016',
+//     entryBody: 'Here is the first entry. It is not great but it is functional.'
+//   },
+//   {
+//     entryTitle: 'Entry 2',
+//     entryDate: 'September 22 2016',
+//     entryBody: 'Here is the second entry. It is not great as great as the first but it is also functional.'
+//   }
+// ];
 
 //STATIC ROUTE
 app.use(express.static('public'));
@@ -36,12 +36,28 @@ app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-//GET ALL ENTRIES
-app.get('/api/entryList', function(req, res) {
-  res.json(entryList)
-})
+
+//GET ALL ENTRIES FROM DB
+app.get('/api/entries', function (req, res) {
+  db.Entry.find(function (err, protips){
+    if (err) {return console.log('get error: ' + err);}
+    res.json(entries);
+  });
+});
 
 
+//JSON ENDPOINTS
+app.get('/api', function api_index(req, res) {
+  res.json({
+    EntriesEndpoints: true,
+    message: 'Here are my endpoints',
+    endpoints: [
+      {method: 'GET', path: '/api', description: 'Homepage'},
+      {method: 'GET', path: 'api/entries', description: 'displays all entries'},
+      {method: 'GET', path: 'api/entries/:id', description: 'displays one entries'}
+    ]
+  })
+});
 
 
 // LISTEN TO LOCALHOST
